192f6ceed7d6cc33b922d4851dc8bf43fdbbf4b3
Marcos Roriz <marcosroriz@ufg.br>
2019-03-27 03:17:39 -0300

    Started implementing entry/login page

--- src/index.js
+++ src/index.js
@@ -1,40 +1,113 @@
-import { app, BrowserWindow } from 'electron';
+const electron = require("electron");
+const { app, BrowserWindow, ipcMain } = electron;
+const path = require('path');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
+function handleSquirrelEvent() {
+  if (process.argv.length === 1) {
+    return false;
+  }
+
+  const ChildProcess = require('child_process');
+  const appFolder = path.resolve(process.execPath, '..');
+  const rootAtomFolder = path.resolve(appFolder, '..');
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+  const exeName = path.basename(process.execPath);
+
+  const spawn = function (command, args) {
+    let spawnedProcess, error;
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
+    } catch (error) { }
+
+    return spawnedProcess;
+  };
+
+  const spawnUpdate = function (args) {
+    return spawn(updateDotExe, args);
+  };
+
+  const squirrelEvent = process.argv[1];
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName]);
+
+      setTimeout(app.quit, 1000);
+      return true;
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      app.quit();
+      return true;
+  }
+};
+
+handleSquirrelEvent()
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let entryWindow;
 
-const createWindow = () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
+const createEntryWindow = () => {
+  // Create the entry window.
+  entryWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    backgroundThrottling: false,
+    show: false
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  // and load the entry.html of the app.
+  entryWindow.loadURL(`file://${__dirname}/entry.html`);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  entryWindow.webContents.openDevTools();
+
+  entryWindow.on("ready-to-show", () => {
+    entryWindow.maximize();
+    entryWindow.show();
+  });
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
+  entryWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null;
+    entryWindow = null;
   });
 };
+
+app.disableHardwareAcceleration(); 
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', createEntryWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -48,10 +121,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
+  if (entryWindow === null) {
+    createEntryWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
---
+++ src/js/entry-script.js
@@ -0,0 +1,163 @@
+window.$ = window.jQuery = require('jquery');
+window.Tether = require('tether');
+window.Bootstrap = require('bootstrap');
+require("jquery-validation");
+require("jquery-mask-plugin");
+
+window.$.validator.addMethod("cpf", function (value, element) {
+    console.log("CHEGUEI AQUI NO CPF");
+    console.log(value);
+    console.log(element);
+
+    value = jQuery.trim(value);
+
+    value = value.replace('.', '');
+    value = value.replace('.', '');
+    let cpf = value.replace('-', '');
+
+    while (cpf.length < 11) cpf = "0" + cpf;
+    let expReg = /^0+$|^1+$|^2+$|^3+$|^4+$|^5+$|^6+$|^7+$|^8+$|^9+$/;
+    let a = [];
+    let b = new Number;
+    let c = 11;
+    for (let i = 0; i < 11; i++) {
+        a[i] = cpf.charAt(i);
+        if (i < 9) b += (a[i] * --c);
+    }
+    let x = b % 11;
+    if (x < 2) {
+        a[9] = 0;
+    } else {
+        a[9] = 11 - x;
+    }
+    b = 0;
+    c = 11;
+    let y = 0;
+    for (y = 0; y < 10; y++) {
+        b += (a[y] * c--);
+    }
+    x = b % 11;
+    if (x < 2) {
+        a[10] = 0;
+    } else {
+        a[10] = 11 - x;
+    }
+
+    let retorno = true;
+    if ((cpf.charAt(9) != a[9]) || (cpf.charAt(10) != a[10]) || cpf.match(expReg)) retorno = false;
+
+    return this.optional(element) || retorno;
+
+}, "Informe um CPF válido");
+
+window.$.validator.addMethod("lettersonly", function (value, element) {
+    return this.optional(element) || /^[a-z áàäéêëíïóõôöúûüùçñ]+$/i.test(value);
+}, "Informe apenas caracteres válidos");
+
+
+$(document).ready(function () {
+    var telmaskbehaviour = function (val) {
+        return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+    };
+    var teloptions = {
+        onKeyPress: function (val, e, field, options) {
+            field.mask(telmaskbehaviour.apply({}, arguments), options);
+        }
+    };
+
+    $('.telmask').mask(telmaskbehaviour, teloptions);
+
+    $(".cpfmask").mask('000.000.000-00', { reverse: true });
+
+    // $("#loginform").validate({
+    //     rules: {
+    //         loginemail: {
+    //             required: true,
+    //             email: true
+    //         },
+    //         loginpassword: {
+    //             required: true
+    //         }
+    //     },
+    //     messages: {
+    //         loginemail: {
+    //             required: "Por favor digite seu endereço de e-mail",
+    //             email: "Por favor digite um endereço de e-mail válido"
+    //         },
+    //         loginpassword: {
+    //             required: "Por favor digite sua senha"
+    //         }
+    //     },
+    //     highlight: function (element) {
+    //         $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+    //         $(element).closest('.form-check').removeClass('has-success').addClass('has-error');
+    //     },
+    //     success: function (element) {
+    //         $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+    //         $(element).closest('.form-check').removeClass('has-error').addClass('has-success');
+    //     },
+    //     errorPlacement: function (error, element) {
+    //         $(element).closest('.form-group').append(error).addClass('has-error');
+    //     }
+    // });
+
+    $("#registerform").validate({
+        rules: {
+            regnome: {
+                required: true,
+                lettersonly: true
+            },
+            regcpf: {
+                required: true,
+                cpf: true
+            },
+            regtel: {
+                required: true,
+                minlength: 10
+            },
+            regemail: {
+                required: true,
+                email: true
+            },
+            regpassword: {
+                required: true
+            },
+            regpasswordrepeat: {
+                required: true,
+                equalTo: "#regpassword"
+            }
+        },
+        messages: {
+            regnome: {
+                required: "Por favor digite seu endereço de e-mail",
+            },
+            regcpf: {
+                required: "Por favor digite sua senha"
+            },
+            regtel: {
+                required: "Por favor digite um telefone válido com DDD"
+            },
+            regemail: {
+                email: "Por favor digite um e-mail válido"
+            },
+            regpassword: {
+                required: "Por favor digite uma senha"
+            },
+            regpasswordrepeat: {
+                required: "Por favor confirme sua senha",
+                equalTo: "As senhas são diferentes"
+            }
+        },
+        highlight: function (element) {
+            $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+            $(element).closest('.form-check').removeClass('has-success').addClass('has-error');
+        },
+        success: function (element) {
+            $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+            $(element).closest('.form-check').removeClass('has-error').addClass('has-success');
+        },
+        errorPlacement: function (error, element) {
+            $(element).closest('.form-group').append(error).addClass('has-error');
+        }
+    });
+});
