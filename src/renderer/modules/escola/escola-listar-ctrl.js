837b4544bd1945985105242086e232d856b58c8a
Marcos Roriz <marcosrorizinf@gmail.com>
2020-12-16 10:27:25 -0300

    Migração inicial telas escola para FB

--- src/renderer/modules/escola/escola-listar-ctrl.js
+++ src/renderer/modules/escola/escola-listar-ctrl.js
@@ -1,77 +1,58 @@
-// Preenchimento da Tabela via SQL
+// escola-listar-ctrl.js
+// Este arquivo contém o script de controle da tela escola-listar-view. O memso
+// apresenta as escoladas cadastras em uma tabela. Para tal, é feito uma consulta
+// das escolas no banco de dados. Também é feito consultas nos dados de alunos para
+// correlacionar o quantitativo de alunos por escola
+
+// Variável que armazena as escolas que serão apresentadas (será preenchida)
 var listaDeEscolas = new Map();
 
-// DataTables
+// Configura o DataTables
 var dataTableEscolas = $("#datatables").DataTable({
-    // fixedHeader: true,
-    columns: [
-        { data: 'NOME', width: "40%" },
-        { data: 'LOCALIZACAO',  width: "15%" },
-        { data: 'ENSINO', width: "15%" },
-        { data: 'HORARIO', width: "30%" },
-        { data: 'NUM_ALUNOS', width: "300px" },
-        {
-            data: "ACOES",
-            width: "110px",
-            sortable: false,
-            defaultContent: '<a href="#" class="btn btn-link btn-info escolaStudent"><i class="fa fa-user"></i></a>' +
-                            '<a href="#" class="btn btn-link btn-primary escolaView"><i class="fa fa-search"></i></a>' +
-                            '<a href="#" class="btn btn-link btn-warning escolaEdit"><i class="fa fa-edit"></i></a>' +
-                            '<a href="#" class="btn btn-link btn-danger escolaRemove"><i class="fa fa-times"></i></a>'
-        }
-    ],
-    columnDefs: [{
-        targets: 0,
-        type: 'locale-compare',
-        render: function (data, type, row) {
-            return data.length > 50 ?
-                data.substr(0, 50) + '…' :
-                data;
-        }
-    }],
-    autoWidth: false,
-    bAutoWidth: false,
-    lengthMenu: [[10, 50, -1], [10, 50, "Todas"]],
-    pagingType: "full_numbers",
-    order: [[ 0, "desc" ]],
-    language: {
-        "search": "_INPUT_",
-        "searchPlaceholder": "Procurar escolas",
-        "lengthMenu": "Mostrar _MENU_ escolas por página",
-        "zeroRecords": "Não encontrei nenhuma escola cadastrada",
-        "info": "Mostrando página _PAGE_ de _PAGES_",
-        "infoEmpty": "Sem registros disponíveis",
-        "infoFiltered": "(Escolas filtradas a partir do total de _MAX_ escolas)",
-        "paginate": {
-            "first":      "Primeira",
-            "last":       "Última",
-            "next":       "Próxima",
-            "previous":   "Anterior"
-        },
-    },
-    dom: 'lfrtipB',
-    buttons: [
-        {
-            extend: 'pdfHtml5',
-            orientation: "landscape",
-            title: "Escolas cadastradas",
-            text: "Exportar para PDF",
-            exportOptions: {
-                columns: [0, 1, 2, 3, 4]
-            },
-            customize: function (doc) {
-                doc.content[1].table.widths = ['30%', '15%', '20%', '20%', '15%'];
-                doc.images = doc.images || {};
-                doc.images["logo"] = baseImages.get("logo");
-                doc.content.splice(1, 0, {
-                    alignment: 'center',
-                    margin: [0, 0, 0, 12],
-                    image: "logo"
-                });
-                doc.styles.tableHeader.fontSize = 12;
+    // A função abaixo inicia nossa pré-configuração do datatable
+    // ver detalhe da função em js/datatable.extra.js
+    ...dtConfigPadraoFem("escola"),
+    ...{
+        columns: [
+            { data: 'NOME', width: "40%" },
+            { data: 'LOCALIZACAO',  width: "15%" },
+            { data: 'ENSINO', width: "15%" },
+            { data: 'HORARIO', width: "30%" },
+            { data: 'NUM_ALUNOS', width: "300px" },
+            {
+                data: "ACOES",
+                width: "110px",
+                sortable: false,
+                defaultContent: '<a href="#" class="btn btn-link btn-info escolaStudent"><i class="fa fa-user"></i></a>' +
+                                '<a href="#" class="btn btn-link btn-primary escolaView"><i class="fa fa-search"></i></a>' +
+                                '<a href="#" class="btn btn-link btn-warning escolaEdit"><i class="fa fa-edit"></i></a>' +
+                                '<a href="#" class="btn btn-link btn-danger escolaRemove"><i class="fa fa-times"></i></a>'
             }
-        }
-    ]
+        ],
+        columnDefs: [{ targets: 0, type: 'locale-compare', render: renderAtMostXCharacters(50) }],
+        buttons: [
+            {
+                extend: 'pdfHtml5',
+                orientation: "landscape",
+                title: "Escolas cadastradas",
+                text: "Exportar para PDF",
+                exportOptions: {
+                    columns: [0, 1, 2, 3, 4]
+                },
+                customize: function (doc) {
+                    doc.content[1].table.widths = ['30%', '15%', '20%', '20%', '15%'];
+                    doc.images = doc.images || {};
+                    doc.images["logo"] = baseImages.get("logo");
+                    doc.content.splice(1, 0, {
+                        alignment: 'center',
+                        margin: [0, 0, 0, 12],
+                        image: "logo"
+                    });
+                    doc.styles.tableHeader.fontSize = 12;
+                }
+            }
+        ]
+    }
 });
 
 dataTableEscolas.on('click', '.escolaStudent', function () {
@@ -103,82 +84,71 @@ dataTableEscolas.on('click', '.escolaRemove', function () {
 
     estadoEscola = dataTableEscolas.row($tr).data();
     action = "apagarEscola";
-    Swal2.fire({
-        title: 'Remover essa escola?',
-        text: "Ao remover uma escola os alunos remanescentes da mesma deverão ser alocados novamente a outra(s) escola(s).",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#d33',
-        cancelButtonColor: '#3085d6',
-        cancelButtonText: "Cancelar",
-        confirmButtonText: 'Sim, remover'
-    }).then((result) => {
+    confirmDialog('Remover essa escola?',
+                  "Ao remover uma escola os alunos remanescentes da mesma " + 
+                  "deverão ser alocados novamente a outra(s) escola(s)."
+    ).then((result) => {
+        let listaPromisePraRemover = []
         if (result.value) {
-            RemoverEscola(estadoEscola["ID_ESCOLA"], (err, result) => {
-                if (result) {
-                    dataTableEscolas.row($tr).remove();
-                    dataTableEscolas.draw();
-                    Swal2.fire({
-                        type: 'success',
-                        title: "Sucesso!",
-                        text: "Escola removida com sucesso!",
-                        confirmButtonText: 'Retornar a página de administração'
-                    });
-                } else {
-                    Swal2.fire({
-                        type: 'error',
-                        title: 'Oops...',
-                        text: 'Tivemos algum problema. Por favor, reinicie o software!',
-                    });
-                }
-            })
+            listaPromisePraRemover.push(dbRemoverDadoPorIDPromise(DB_TABLE_ESCOLA, "ID_ESCOLA", estadoEscola["ID"]));
+            listaPromisePraRemover.push(dbRemoverDadoSimplesPromise(DB_TABLE_ESCOLA_TEM_ALUNOS, "ID_ESCOLA", estadoEscola["ID"]));
+            listaPromisePraRemover.push(dbRemoverDadoSimplesPromise(DB_TABLE_ROTA_PASSA_POR_ESCOLA, "ID_ESCOLA", estadoEscola["ID"]));
+            listaPromisePraRemover.push(dbAtualizaVersao());
         }
-    })
+
+        return Promise.all(listaPromisePraRemover)
+    }).then((res) => {
+        if (res.length > 0) {
+            dataTableEscolas.row($tr).remove();
+            dataTableEscolas.draw();
+            Swal2.fire({
+                title: "Sucesso!",
+                icon: "success",
+                text: "Aluno(a) removido(a) com sucesso!",
+                confirmButtonText: 'Retornar a página de administração'
+            });
+        }
+    }).catch((err) => errorFn("Erro ao remover a escola", err))
 });
 
-// Função para relatar erro
-var errorFnEscolas = (err) => {
-    Swal2.fire({
-        title: "Ops... tivemos um problema!",
-        text: "Erro ao listar as escolas! Feche e abra o software novamente. \n" + err,
-        icon: "error",
-        button: "Fechar"
-    });
+
+dbBuscarTodosDadosPromise(DB_TABLE_ESCOLA)
+.then(res => preprocessarEscolas(res))
+.then(() => dbBuscarTodosDadosPromise(DB_TABLE_ESCOLA_TEM_ALUNOS))
+.then((res) => preprocessarRelacaoEscolaAluno(res))
+.then((res) => adicionaDadosTabela(res))
+.catch((err) => errorFn("Erro ao listar as escolas!", err))
+
+// Preprocessa alunos
+var preprocessarEscolas = (res) => {
+    $("#totalNumEscolas").text(res.length);
+    for (let escolaRaw of res) {
+        let escolaJSON = parseEscolaDB(escolaRaw);
+        escolaJSON["NUM_ALUNOS"] = 0;
+        listaDeEscolas.set(escolaJSON["ID"], escolaJSON);
+    }
+    return listaDeEscolas;
 }
 
-// Callback para pegar número de alunos da escola
-var listaNumAlunosCB = (err, result) => {
-    if (err) {
-        errorFnEscolas(err);
-    } else {
-        for (let escolaRaw of result) {
-            let eID = escolaRaw["ID_ESCOLA"];
-            let qtde = escolaRaw["NUM_ALUNOS"];
+// Preprocessa relação de escolas e alunos para pegar o quantitativo entre eles
+var preprocessarRelacaoEscolaAluno = (res) => {
+    for (let relEscolaAluno of res) {
+        let eID = relEscolaAluno["ID_ESCOLA"];
+        if (listaDeEscolas.has(eID)) {
             let escolaJSON = listaDeEscolas.get(eID);
-            escolaJSON["NUM_ALUNOS"] = qtde;
+            escolaJSON["NUM_ALUNOS"] = escolaJSON["NUM_ALUNOS"] + 1;
             listaDeEscolas.set(eID, escolaJSON);
         }
-
-        listaDeEscolas.forEach((escola) => {
-            dataTableEscolas.row.add(escola);
-        });
-
-        dataTableEscolas.draw();
     }
-};
 
-// Callback para pegar dados inicia da escolas
-var listaInicialCB = (err, result) => {
-    if (err) {
-        errorFnEscolas(err);
-    } else {
-        $("#totalNumEscolas").text(result.length);
-        for (let escolaRaw of result) {
-            let escolaJSON = parseEscolaDB(escolaRaw);
-            listaDeEscolas.set(escolaJSON["ID_ESCOLA"], escolaJSON);
-        }
-        NumeroDeAlunosEscolas(listaNumAlunosCB);
-    }
-};
+    return listaDeEscolas;
+}
 
-BuscarTodasEscolas(listaInicialCB);
+// Adiciona dados na tabela
+adicionaDadosTabela = (res) => {
+    res.forEach((escola) => {
+        dataTableEscolas.row.add(escola);
+    });
+
+    dataTableEscolas.draw();
+}
