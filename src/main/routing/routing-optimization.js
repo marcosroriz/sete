1f24f584ecb6f440a325241e8e4f2e2b3d051f60
Marcos Roriz <marcosrorizinf@gmail.com>
2021-05-02 13:57:24 -0300

    Reestruturação das promessas

--- src/main/routing/routing-optimization.js
+++ src/main/routing/routing-optimization.js
@@ -32,77 +32,99 @@ module.exports = class RoutingOptimization {
     optimize() {
         return new Promise((resolve, reject) => {
             // Activate spatial db
-            this.spatialiteDB.spatialite((error) => {
+            this.spatialiteDB.spatialite((dbError) => {
+                if (dbError) {
+                    reject("ERRO AO ABRIR MALHA");
+                }
+
+                let routers = new Array();
+                let busRoutes = new Array();
                 let kmeans = new SchoolBusKMeans(this.routingParams);
+                let routingGraph;
+
                 kmeans.partition(this.routingParams["numVehicles"])
-                    .then((clusters) => {
-                        var clusterizedStops = new Array();
+                    .then(clusters => {
+                        let clusterizedStops = new Array();
                         clusters.forEach((c) => clusterizedStops.push(this.getStops(c.cluster)))
 
-                        var clarkAlgorithmsPromise = new Array();
-                        var routers = new Array();
+                        let clarkAlgorithmsPromise = new Array();
                         clusterizedStops.forEach((cs) => {
-                            var param = Object.assign({}, this.routingParams);
+                            let param = Object.assign({}, this.routingParams);
                             param["stops"] = cs;
 
-                            var cwalg = new ClarkeWrightSchoolBusRouting(param, this.spatialiteDB);
+                            // Deixar apenas as escolas que atendem os alunos no conjunto
+                            let clusterSchoolsSet = new Set()
+                            cs.forEach(student => clusterSchoolsSet.add(student["school"]))
+                            let clusterSchools = new Array()
+                            this.routingParams.schools.forEach(school => {
+                                if (clusterSchoolsSet.has(school["key"])) {
+                                    clusterSchools.push(school);
+                                }
+                            })
+                            param["schools"] = clusterSchools;
+
+                            let cwalg = new ClarkeWrightSchoolBusRouting(param, this.spatialiteDB);
                             clarkAlgorithmsPromise.push(cwalg.spatialRoute());
                             routers.push(cwalg);
                         })
 
                         // let schoolBusRouter = new ClarkeWrightSchoolBusRouting(this.routingParams, this.spatialiteDB);
                         // schoolBusRouter.spatialRoute().then((busRoutes) => {
-                        Promise.all(clarkAlgorithmsPromise).then((busRoutes) => {
-                            // Run opt
-                            let optimizedRoutes = new Array();
+                        return Promise.all(clarkAlgorithmsPromise)
+                    })
+                    .then((busRoutesGenerated) => {
+                        // Bus Routes
+                        busRoutes = busRoutesGenerated;
 
-                            // Compute Route JSON (need to run at promise)
-                            var promises = new Array();
+                        // Routing Graph
+                        var matrixMap = new Map();
+                        routers.forEach((alg) => {
+                            matrixMap = new Map([...matrixMap, ...alg.graph.matrix])
+                        })
+                        routingGraph = routers[0].graph;
+                        routingGraph.setMatrix(matrixMap);
 
-                            // Routing Graph
-                            var matrixMap = new Map();
-                            routers.forEach((alg) => {
-                                matrixMap = new Map([...matrixMap, ...alg.graph.matrix])
+                        return Promise.all(routingGraph.buildSpatialMatrix())
+                    })
+                    .then(() => {
+                        // Run opt
+                        let optimizedRoutes = new Array();
+
+                        // Compute Route JSON (need to run at promise)
+                        var promises = new Array();
+
+                        // Iterate result
+                        let i = 0;
+                        for (i = 0; i < busRoutes.length; i++) {
+                            var genRoutes = busRoutes[i];
+                            // Print Routes
+                            console.log(genRoutes);
+                            genRoutes.forEach((r) => {
+                                console.log("ANTES", r.route)
+                                let optRoute = new TwoOpt(r, routingGraph).optimize();
+                                console.log("DEPOIS", optRoute.route)
+                                optimizedRoutes.push(optRoute);
                             })
-                            var routingGraph = routers[0].graph;
-                            routingGraph.setMatrix(matrixMap);
-                            Promise.all(routingGraph.buildSpatialMatrix())
-                                .then(() => {
-                                    // Iterate result
-                                    let i = 0;
-                                    for (i = 0; i < busRoutes.length; i++) {
-                                        var genRoutes = busRoutes[i];
-                                        // Print Routes
-                                        console.log(genRoutes);
-                                        genRoutes.forEach((r) => {
-                                            console.log("ANTES", r.route)
-                                            let optRoute = new TwoOpt(r, routingGraph).optimize();
-                                            console.log("DEPOIS", optRoute.route)
-                                            optimizedRoutes.push(optRoute);
-                                        })
 
-                                        optimizedRoutes.forEach((r) => {
-                                            promises.push(r.toPlainJSON(routingGraph, this.spatialiteDB));
-                                        });
-                                    }
+                            optimizedRoutes.forEach((r) => {
+                                promises.push(r.toPlainJSON(routingGraph, this.spatialiteDB));
+                            });
+                        }
 
-                                    Promise.all(promises).then((routesJSON) => {
-                                        var fc = new Map();
-                                        routesJSON.forEach((r) => {
-                                            var ckey = r["path"].map(a => a["id"]).join("-")
-                                            fc.set(ckey, r);
-                                        })
-                                        console.log([...fc.values()])
-                                        resolve([...fc.values()])
-                                    })
-                                })
-
-
-                        });
-                    });
-            });
+                        return Promise.all(promises)
+                    })
+                    .then((routesJSON) => {
+                        var fc = new Map();
+                        routesJSON.forEach((r) => {
+                            var ckey = r["path"].map(a => a["id"]).join("-")
+                            fc.set(ckey, r);
+                        })
+                        console.log([...fc.values()])
+                        resolve([...fc.values()])
+                    })
+            })
         });
-    }
+    };
 }
 
                //  schoolBusRouter.buildDistMatrix()
