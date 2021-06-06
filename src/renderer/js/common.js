c41eef87e76bfe56d71c63454a1ab50ea8ebc319
Marcos Roriz <marcosrorizinf@gmail.com>
2021-06-06 15:28:54 -0300

    Update common.js

--- src/renderer/js/common.js
+++ src/renderer/js/common.js
@@ -9,9 +9,9 @@
 // - funções para criar notificações (erro, pergunta, etc) com SweetAlert2
 // - funções que retornam as opções comuns dos wizards e de validação dos formulários
 
-alert("Oi");
+alert("Teste");
 function k(a,b) {
-   console.log(a+b)
+   console.log(a*b)
 }
 
 // Imports Principais do Electron
