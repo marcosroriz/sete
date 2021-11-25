49fe456307b65e0fa88263341ebbd2b4cd262699
Marcos Roriz <marcosrorizinf@gmail.com>
2021-11-25 15:51:44 -0300

    Fix no filtro das colunas

--- src/renderer/js/datatable.extra.js
+++ src/renderer/js/datatable.extra.js
@@ -206,7 +206,7 @@ function dtInitFiltros(dt, colunas) {
             } else {
                 select.on('change', function (evt) {
                     var val = $.fn.dataTable.util.escapeRegex($(this).val());
-                    column.search(val ? val + '$' : '', true, false).draw();
+                    column.search(val ? val : '', true, false).draw();
                 });
 
                 column.data().unique().sort().each(function (d, j) {
