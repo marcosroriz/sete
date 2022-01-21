80b37066209a913959ed02db4d0702f50f49c0b2
Marcos Roriz <marcosrorizinf@gmail.com>
2022-01-21 14:27:41 -0300

    Adição inicial da ferramenta de custo rodo

---
+++ src/renderer/modules/custo/custo-resultado-rodo-ctrl.js
@@ -0,0 +1,7 @@
+// custo-resultado-ctrl.js
+// Este arquivo contém o script de controle da tela custo-resultado-view. 
+// A ferramenta ilustra o resultado da operação de cálculo de custo de uma rota do sistema
+
+// Wizard
+$('.card-wizard').bootstrapWizard(configWizardBasico("", usarValidador = false))
+
